93afbe38ade77957a3a1aeb062123a0bff9b7082
ravigod22 <ravi22135@gmail.com>
2024-04-10 09:51:16 -0500

    PracticaCalificada1

--- Ejercicio2/Tarea2-CC3S2/src/main/java/io/collective/SimpleAgedCache.java
+++ Ejercicio2/Tarea2-CC3S2/src/main/java/io/collective/SimpleAgedCache.java
@@ -9,13 +9,27 @@ public class SimpleAgedCache {
     private final String[] cache_key = new String[2000];
     private final String[] cache_value = new String[2000];
     private final int[] cache_times = new int[2000];
-    private int times;
-    private String key;
-    private String value;
-    Clock clock = new Clock();
+    private Clock clock = new Clock() {
+        @Override
+        public ZoneId getZone() {
+            return null;
+        }
+
+        @Override
+        public Clock withZone(ZoneId zone) {
+            return null;
+        }
+
+        @Override
+        public Instant instant() {
+            return null;
+        }
+    };
 
     int contador = 0;
-
+    public SimpleAgedCache() {
+        this.contador = 0;
+    }
     public SimpleAgedCache(Clock clock) {
         this.clock = clock;
     }
@@ -31,8 +45,26 @@ public class SimpleAgedCache {
     public int size() {
         return contador;
     }
+    public String getKey(int pos) {
+        return cache_key[pos];
+    }
     public String get(String key) {
         if (contador == 0) return null;
+        int pos = 0;
+        for (int i = 0; i < contador; ++i) {
+            if (getKey(i).equals(key)) {
+                pos = i;
+                break;
+            }
+        }
+        return cache_value[pos];
+    }
 
+    public Clock getClock() {
+        return clock;
+    }
+
+    public void setClock(Clock clock) {
+        this.clock = clock;
     }
 }
